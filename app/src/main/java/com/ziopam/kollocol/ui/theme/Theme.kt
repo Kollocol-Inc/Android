8a9ae981e481dc7205795c8cb42b1a6db1dd3fdb
ziopam <ppd.orel@mail.ru>
2026-01-06 17:13:57 +0300

    remove useless extra colors

--- app/src/main/java/com/ziopam/kollocol/ui/theme/Theme.kt
+++ app/src/main/java/com/ziopam/kollocol/ui/theme/Theme.kt
@@ -3,30 +3,21 @@ package com.ziopam.kollocol.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.staticCompositionLocalOf
 
 const val MAX_BUTTON_WIDTH = 320
 const val MAX_EDITTEXT_WIDTH = 488
-
-val LocalExtraColors = staticCompositionLocalOf<ExtraColors> {
-    error("ExtraColors not provided")
-}
 
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val appColors = if (darkTheme) DarkExtraColors else LightExtraColors
     val materialColors = if (darkTheme) DarkMaterialScheme else LightMaterialScheme
 
     MaterialTheme(
         colorScheme = materialColors,
         typography = Typography,
     ) {
-        androidx.compose.runtime.CompositionLocalProvider(
-            LocalExtraColors provides appColors,
-            content = content
-        )
+        content()
     }
 }
